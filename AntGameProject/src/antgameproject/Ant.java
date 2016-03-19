be3cc38e1b69b7810779f46fa48893eb5f64b4dd
wilki <wilki@DESKTOP-DGB6O1R>
2016-03-19 21:41:21 +0000

    Created an Ant class, requires methods to be implemented.

---
+++ AntGameProject/src/antgameproject/Ant.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antgameproject;
+
+/**
+ *
+ * @author wilki
+ */
+public class Ant {
+    private final Color antColor;
+    private int currentBrainState;
+    private final int id;
+    private int resting;
+    private Direction facingDirection;
+    private boolean hasFood;
+    private Pos boardPosition;
+    
+    public Ant(Color antColor, int id, Pos boardPosition){
+        this.antColor = antColor;
+        currentBrainState = 0;
+        this.id = id;
+        resting = 0;
+        // facing direction 0?
+        hasFood = false;
+        this.boardPosition = boardPosition;
+    }
+}
