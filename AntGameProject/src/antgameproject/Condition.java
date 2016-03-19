9dbd9372e068b0b0994e340f21ee59d258a410f9
wilki <wilki@DESKTOP-DGB6O1R>
2016-03-19 21:19:12 +0000

    Created enums for condition, direction and marker.

---
+++ AntGameProject/src/antgameproject/Condition.java
@@ -0,0 +1,16 @@
+package antgameproject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wilki
+ */
+public enum Condition {
+    FRIEND, FOE, FRIENDWITHFOOD, FOEWITHFOOD, FOOD, ROCK, MARKER, FOEMARKER,
+    HOME, FOEHOME;
+}
---
+++ AntGameProject/src/antgameproject/Marker.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antgameproject;
+
+/**
+ *
+ * @author wilki
+ */
+public enum Marker {
+    MARKER1, MARKER2, MARKER3, MARKER4, MARKER5, MARKER6; 
+}
