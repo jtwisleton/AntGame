55d8df13ec59911be4e1915ab5240d33a6b81fd9
wilki <wilki@DESKTOP-DGB6O1R>
2016-03-28 14:00:38 +0400

    Empty marker added to the list.

--- AntGameProject/src/antgameproject/Marker.java
+++ AntGameProject/src/antgameproject/Marker.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package antgameproject;
 
 /**
@@ -10,5 +6,5 @@ package antgameproject;
  * @author wilki
  */
 public enum Marker {
-    MARKER1, MARKER2, MARKER3, MARKER4, MARKER5, MARKER6; 
+    MARKER1, MARKER2, MARKER3, MARKER4, MARKER5, MARKER6, EMPTY; 
 }
