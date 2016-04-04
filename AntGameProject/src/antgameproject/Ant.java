ca755a4c26f822d100fc21e1d3be434bba5f8c25
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-04 21:17:13 +0100

    Added more methods to the Ant class.

--- AntGameProject/src/antgameproject/Ant.java
+++ AntGameProject/src/antgameproject/Ant.java
@@ -10,24 +10,25 @@ package antgameproject;
  * @author wilki
  */
 public class Ant {
-    private final Colour antColor;
+    private final Colour antColour;
     private int currentBrainState;
     private final int id;
     private int resting;
-    private Direction facingDirection;
-    private boolean hasFood;
+    private int facingDirection;
+    private boolean carryingFood;
     private Pos boardPosition;
     private boolean antIsAlive;
     
-    public Ant(Colour antColor, int id, Pos boardPosition){
-        this.antColor = antColor;
+    public Ant(Colour antColour, int id, Pos boardPosition){
+        this.antColour = antColour;
         currentBrainState = 0;
         this.id = id;
         resting = 0;
         // facing direction 0?
-        hasFood = false;
+        carryingFood = false;
         this.boardPosition = boardPosition;
         antIsAlive = true;
+        facingDirection = 0;
     }
     
     public boolean getAntIsAlive(){
@@ -44,5 +45,50 @@ public class Ant {
     
     public void setBoardPosition(Pos newPosition){
         this.boardPosition = newPosition;
+    }
+    
+    public boolean antIsResting(){
+        return resting > 0;
+    }
+    
+    public void decreaseAntResting(){
+        resting--;
+    }
+    
+    public Colour getAntColour(){
+        return antColour;
+    }
+    
+    public int getCurrentBrainState(){
+        return currentBrainState;
+    }
+    
+    public void setBrainState(int brainState){
+        currentBrainState = brainState;
+    }
+    
+    public boolean getCarryingFood(){
+        return carryingFood;
+    }
+    
+    public void setCarryingFood(boolean carryingFood){
+        this.carryingFood = carryingFood;
+    }
+    
+    // double check directions
+    public void setTurnDirection(TurnDirection directionToTurn){
+        if(directionToTurn == TurnDirection.RIGHT){
+            facingDirection = (facingDirection + 1) % 6;
+        } else {
+            facingDirection = (facingDirection - 1) % 6;
+        } 
+    }
+    
+    public int getFacingDirection(){
+        return facingDirection;
+    }
+    
+    public void setResting(int timeToRest){
+        resting = timeToRest;
     }
 }
