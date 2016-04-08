c93de9a4018a98002096eea156e89f4e724eb51c
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-08 11:26:37 +0100

    Created marker class that implements Condition interface.

--- AntGameProject/src/antgameproject/Marker.java
+++ AntGameProject/src/antgameproject/Marker.java
@@ -5,6 +5,17 @@ package antgameproject;
  *
  * @author wilki
  */
-public enum Marker {
-    MARKER1, MARKER2, MARKER3, MARKER4, MARKER5, MARKER6, EMPTY; 
+public class Marker implements Condition{
+    private final int markerType;
+    
+    public Marker(int markerType){
+        assert markerType < 6 && markerType >= 0;
+        this.markerType = markerType;
+    }
+
+    @Override
+    public boolean testCondition(Pos sensePosition, Colour antColour, Board gameBoard) {
+        return gameBoard.checkMarker(sensePosition, antColour, markerType);
+    }
+    
 }
