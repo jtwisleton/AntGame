9b42e20e1a88c6ebe6e89499443b1becd84d3486
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-10 18:48:38 +0100

    Created test class for Flip class.

---
+++ AntGameProject/test/antGameTesting/FlipJUnitTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antGameTesting;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wilki
+ */
+public class FlipJUnitTest {
+    
+  
+    
+    @Before
+    public void setUp() {
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void tempTest(){
+        assertTrue(true);
+    }
+}
