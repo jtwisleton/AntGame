defe3e9bae6faa9d1737df282b6a4f3daa50ac9b
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-10 18:48:16 +0100

    Created test class for Sense class.

---
+++ AntGameProject/test/antGameTesting/SenseJUnitTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antGameTesting;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wilki
+ */
+public class SenseJUnitTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+   @Test
+   public void tempTest(){
+       assertTrue(true);
+   }
+
+}
