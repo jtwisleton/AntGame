a4c3a8b83634c04ea2a5b787f72060625d69642c
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-10 22:05:17 +0100

    Overwritten toString method of Terrain class to aid in testing.

--- AntGameProject/src/antgameproject/Terrain.java
+++ AntGameProject/src/antgameproject/Terrain.java
@@ -6,5 +6,16 @@ package antgameproject;
  * @author wilki
  */
 public enum Terrain {
-    GRASS, ROCK, BLACKBASE, REDBASE;
+    GRASS("-"), ROCK("#"), BLACKBASE("+"), REDBASE("-");
+    
+    private final String symbol;
+    
+    Terrain(String symbol){
+        this.symbol = symbol;
+    }
+    
+    @Override
+    public String toString(){
+        return symbol;
+    }
 }
