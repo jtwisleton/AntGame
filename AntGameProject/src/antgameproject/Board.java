646f18eba1c5f998811adb60bc22322d72d416df
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-11 07:36:14 +0100

    Added a print board method and fixed a bug in the addAnts method.

--- AntGameProject/src/antgameproject/Board.java
+++ AntGameProject/src/antgameproject/Board.java
@@ -21,27 +21,31 @@ public class Board {
     public Board(BoardTile[][] board){
         this.board = board;
         antsOnBoard = new ArrayList<>();
-        addAnts();
+        
         colourToBaseMatch = new HashMap<>();
         colourToBaseMatch.put(Colour.BLACK, Terrain.BLACKBASE);
         colourToBaseMatch.put(Colour.RED, Terrain.REDBASE);
+        
+        printBoardToASCII();    // needs to be removed once proram complete
+        addAnts();
+        printBoardToASCII();    // needs to be removed once program complete
     }
     
-    private void addAnts(){        
+    private void addAnts(){
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){                
+            for(int j = 0; j < board[i].length; j++){
                 if(board[i][j].getCellTerrain() != Terrain.ROCK && 
                         board[i][j].getCellTerrain() != Terrain.GRASS){
-                    Colour antColour = null;
                     for(Colour key: colourToBaseMatch.keySet()){
                         if(colourToBaseMatch.get(key) == board[i][j].getCellTerrain()){
-                            antColour = key;
+                            Colour antColour = key;
+                            int antId = antsOnBoard.size();
+                            Pos antPosition = new Pos(i, j);
+                            Ant antToAdd = new Ant(antColour, antId, antPosition);
+                            antsOnBoard.add(antToAdd);
+                            board[i][j].setAntOnTile(antToAdd);
                         }
                     }
-                    int antId = antsOnBoard.size();
-                    Pos antPosition = new Pos(i, j);
-                    Ant antToAdd = new Ant(antColour, antId, antPosition);
-                    antsOnBoard.add(antToAdd);
                 }
             }
         }
@@ -129,6 +133,15 @@ public class Board {
     // Possibly duplicate class of numberOfFoodAt?
     public int getFoodAtint(Pos tilePosition){
         return board[tilePosition.getPosY()][tilePosition.getPosX()].getFoodInTile();
+    }
+    
+    public void printBoardToASCII(){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.print("\n");
+        }
     }
     
 }
