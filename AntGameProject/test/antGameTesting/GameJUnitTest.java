045e20bbcdc67dd765492c0106869796ac36b6c8
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-15 11:43:56 +0100

    Created a class for testing the Game class.

---
+++ AntGameProject/test/antGameTesting/GameJUnitTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antGameTesting;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wilki
+ */
+public class GameJUnitTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void testGameCreation(){
+        
+    }
+}
