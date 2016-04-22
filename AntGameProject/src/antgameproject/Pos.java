559160b4f1ab08b78bbf75975f759e84b30fed77
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-22 22:38:18 +0100

    Added Javadoc to the Pos class.

--- AntGameProject/src/antgameproject/Pos.java
+++ AntGameProject/src/antgameproject/Pos.java
@@ -1,27 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package antgameproject;
 
 /**
- *
- * @author wilki
+ * the Pos class is a wrapper class for a two dimensional position.
  */
 public class Pos {
     private int posX;
     private int posY;
     
+    /**
+     * Constructor for the Pos class.
+     * @param posX x value.
+     * @param posY y value.
+     */
     public Pos(int posX, int posY){
         this.posX = posX;
         this.posY = posY;
     }
     
+    /**
+     * Get the x value of the position.
+     * @return x value.
+     */
     public int getPosX(){
         return posX;
     }
     
+    /**
+     * Get the y value of the position.
+     * @return y value.
+     */
     public int getPosY(){
         return posY;
     }
