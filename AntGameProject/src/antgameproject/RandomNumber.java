e507a4511e6f8931ee90120238dfce6f0147dae4
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-22 22:45:11 +0100

    Added Javadoc to the RandomNumber class.

--- AntGameProject/src/antgameproject/RandomNumber.java
+++ AntGameProject/src/antgameproject/RandomNumber.java
@@ -2,13 +2,16 @@
 package antgameproject;
 
 /**
- *
- * @author jw478
+ * The RandomNumber class is a pseudo random number generator.
  */
 public class RandomNumber {
     
     private int s4;
     
+    /**
+     * Constructor for the RandomNumber class which takes a seed.
+     * @param seed seed for random number generation.
+     */
     public RandomNumber(int seed){
         s4 = seed; 
         for(int i = 0; i < 4; i++){
@@ -16,10 +19,16 @@ public class RandomNumber {
         }
     }
     
-    public int generateNumber(int n){
+    /**
+     * Generates a random number in a range 0 to the upper value given, not 
+     * inclusive of upper value.
+     * @param upperValuespecifies the upper value for the random number generated.
+     * @return a random number in the given range.
+     */
+    public int generateNumber(int upperValue){
         int x = (s4 / 65536) % 16384;
         s4 = (s4 * 22695477 + 1) & (int)(Math.pow(2, 32) - 1);
-        return x % n;
+        return x % upperValue;
     }
 
 }
