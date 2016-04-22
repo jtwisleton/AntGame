d301e3c3b8266bf089f2eee0f42d472d63819f20
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-22 21:11:28 +0100

    Updated Javadoc on the AntGameTournament class.

--- AntGameProject/src/antgameproject/AntGameTournament.java
+++ AntGameProject/src/antgameproject/AntGameTournament.java
@@ -4,14 +4,11 @@ import antgameproject.AntBrainLoader.AntBrainLoaderException;
 import antgameproject.AntWorldLoader.AntWorldLoaderException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Class to represent a tournament of ant games.
- * 
- * @author Joseph Kimberley
  */
 public class AntGameTournament {
  
@@ -22,118 +19,17 @@ public class AntGameTournament {
     private int boardToPlayIndex;
 
     /**
-     * Static class to represent an AntBrain pairing.
-     * 
-     * @param <X> Type of the first entry.
-     * @param <Y> Type of the second entry.
+     * Empty constructor for creating an ant game tournament.
      */
-    public static class Pair<X, Y> {
-        public final AntBrain one;
-        public final AntBrain two;
-        
-        /**
-         * Class constructor for a pair of ant brains.
-         * 
-         * @param one First ant brain.
-         * @param two Second ant brain.
-         */
-        public Pair(AntBrain one, AntBrain two) {
-            this.one = one;
-            this.two = two;
-        }
-    }
-    
     public AntGameTournament(){
         antWorlds = new ArrayList<>();
         antBrains = new ArrayList<>();
         pairs = new ArrayList<>();
         boardToPlayIndex = 0;
     }
-    
-    /**
-     * Class constructor for a list of ant brain files and a list of ant world files.
-     * 
-     * @param antBrainFiles Collection of ant brain files.
-     * @param antWorldFiles Collection of ant world files.
-     */
-    /*
-    public AntGameTournament(List<String> antBrainFiles, List<String> antWorldFiles) {
-        antBrains = loadAntBrains(antBrainFiles);
-        antWorlds = loadAntWorlds(antWorldFiles);
-        pairs = new ArrayList<>();
-        
-        // Initialise each ant brain's score to 0
-        scores = new HashMap<>();
-        for (AntBrain brain : antBrains) {
-            scores.put(brain, 0);
-        }
-        topScore = 0;
-    }
-    */
-    /**
-     * Constructor for a list of ant brain files and a number of ant worlds.
-     * 
-     * @param antBrainFiles Collection of ant brain files.
-     * @param numAntWorlds Number of ant worlds to be generated.
-     */
-    /*
-    public AntGameTournament(List<String> antBrainFiles, int numAntWorlds) {
-        antBrains = loadAntBrains(antBrainFiles);
-        for (int i = 0; i < numAntWorlds; i++) {
-            // Generate ant world
-            // Check ant world and add to array
-        }
-        pairs = new ArrayList<>();
-        
-        // Initialise each ant brain's score to 0
-        scores = new HashMap<>();
-        for (AntBrain brain : antBrains) {
-            scores.put(brain, 0);
-        }
-        topScore = 0;
-    }
-    */
-    /**
-     * Run the tournament simulation.
-     */
-    /*
-    public void runTournament() {
-        // Find every combination of ant brains (AB == BA)
-        for (int i = 0; i < antBrains.size() - 1; i++) {
-            for (int j = i + 1; j < antBrains.size(); j++) {
-                pairs.add(new Pair(antBrains.get(i), antBrains.get(j)));
-            }
-        }
-        
-        // Run a game for each pair twice on each world
-        for (Board antWorld : antWorlds) {
-            for (Pair pair : pairs) {
-                runGame(pair.one, pair.two, antWorld);
-                runGame(pair.two, pair.one, antWorld);
-            }
-        }
-        
-        // Remove all ant brains that have less than the top score
-        Iterator<AntBrain> it = antBrains.iterator();
-        while (it.hasNext()) {
-            AntBrain antBrain = it.next();
-            if (scores.get(antBrain) < topScore) {
-                it.remove();
-            }
-        }
-        
-        // If there are more than one ant brain with the top score, repeat tournament
-        if (antBrains.size() > 1) {
-            // Could reset scores to 0
-            runTournament();
-        }
-        
-        // Do something with winning ant brain: antBrains.get(0)
-    }
-    */
+   
     /**
      * Run a game simulation.
-     * 
      * @param one First ant brain.
      * @param two Second ant brain.
      * @param world World to play game on.
@@ -146,8 +42,7 @@ public class AntGameTournament {
         // Update scores with 2 points for a win and 1 point for a draw
         int playerOneScore = currentGame.getPlayerOneScore();
         int playerTwoScore = currentGame.getPlayerTwoScore();
-        
-        
+                
         one.incrementGamesPlayedIn();
         two.incrementGamesPlayedIn();
         one.setTotalFoodInBase(one.getTotalFoodInBase() + playerOneScore);
@@ -170,56 +65,21 @@ public class AntGameTournament {
         }
         
     }
-    
+
     /**
-     * Load a list of ant brains from a list of files.
-     * 
-     * @param antBrainFiles List of ant brain files.
-     * @return A list of ant brains.
+     * Runs a single game. Two valid ant brains and one valid ant world must have 
+     * already been loaded to this tournament. 
      */
-    private List<AntBrain> loadAntBrains(List<String> antBrainFiles) {
-        List<AntBrain> brains = new ArrayList<>();
-        
-        // For each file, try to load the ant brain and catch any exceptions
-        for (String antBrainFile : antBrainFiles) {
-            try {
-                brains.add(AntBrainLoader.loadBrain(antBrainFile, " "));
-            } catch (IOException | AntBrainLoaderException e) {
-                System.out.println(e);
-            }
-        }
-        
-        return brains;
-    }
-    
-    /**
-     * Load a list of ant worlds from a list of ant world files.
-     * 
-     * @param antWorldFiles List of ant world files.
-     * @return A list of ant worlds.
-     */
-    /*
-    private List<Board> loadAntWorlds(List<String> antWorldFiles) {
-        List<Board> worlds = new ArrayList<>();
-        
-        // For each file, try to load the ant world and catch any exceptions
-        for (String antWorldFile : antWorldFiles) {
-            try {
-                worlds.add(AntWorldLoader.loadWorld(antWorldFile, true));
-            } catch (IOException | AntWorldLoaderException e) {
-                System.out.println(e);
-            }
-        }
-        
-        return worlds;
-    }
-    */
     public void runGame(){
-        // add checks
-        currentGame = new Game(antBrains.get(0), antBrains.get(1), antWorlds.get(0));
+        if(antBrains.size() == 2 && antWorlds.size() == 1){
+            currentGame = new Game(antBrains.get(0), antBrains.get(1), antWorlds.get(0));
+        }
     }
-    
-    // Once called should not allow more brains or world to be added
+
+    /**
+     * Called once all ant brains and worlds have been added to the tournament. 
+     * Sets up the ant brains into pairs to compete against each other. 
+     */
     public void createTournament(){
         for (int i = 0; i < antBrains.size() - 1; i++) {
             for (int j = i + 1; j < antBrains.size(); j++) {
@@ -228,6 +88,10 @@ public class AntGameTournament {
         }
     }
     
+    /**
+     * Runs one round of a tournament. A round means all teams play all enemies
+     * on a board as both the red and the black colours.
+     */
     public void runTournamentRound(){
         Board boardToPlayRoundOn = antWorlds.get(boardToPlayIndex);
         
@@ -241,33 +105,95 @@ public class AntGameTournament {
         } else {
             System.out.println("gameComplete");
         }
+        
+        // sort the ant brain list so in order of points
+        Collections.sort(antBrains);
     }
     
-    public void loadAntWorld(String antWorldFilePath, String antWorldName) throws AntWorldLoaderException, IOException {
+    /**
+     * Loads an ant world from the given file path and gives it the specified name.
+     * @param antWorldFilePath file path to load ant world from.
+     * @param antWorldName name to give the ant world.
+     * @throws AntWorldLoaderException
+     * @throws IOException
+     */
+    public void loadAntWorld(String antWorldFilePath, String antWorldName) 
+            throws AntWorldLoaderException, IOException {
         antWorlds.add(AntWorldLoader.loadWorld(antWorldFilePath, antWorldName, false));    // change back to true as some point
     }
     
-    public void loadAntBrain(String antBrainFilePath, String name) throws AntBrainLoaderException, IOException{
+    /**
+     * Loads an ant brain from the given file path and gives it the specified name.
+     * @param antBrainFilePath file path to load ant brain from.
+     * @param name name to give the ant brain.
+     * @throws AntBrainLoaderException
+     * @throws IOException
+     */
+    public void loadAntBrain(String antBrainFilePath, String name) 
+            throws AntBrainLoaderException, IOException{
         antBrains.add(AntBrainLoader.loadBrain(antBrainFilePath, name));
     }
     
+    /**
+     * Returns a list of all the ant worlds in this tournament.
+     * @return list of all ant worlds in the tournament.
+     */
     public List getListOfAntWorlds(){
         return antWorlds;
     }
     
+    /**
+     * Returns a list of all the ant brains in this tournament.
+     * @return list of all ant brains in the tournament.
+     */
     public List getListOfAntBrains(){
         return antBrains;
     }
     
+    /**
+     * Returns the current game that is being played.
+     * @return current game being played.
+     */
     public Game getCurrentGame(){
         return currentGame;
     }
     
+    /**
+     * Resets the tournament.
+     */
     public void reset(){
         antWorlds = new ArrayList<>();
         antBrains = new ArrayList<>();
         pairs = new ArrayList<>();
         currentGame = null;
+    }
+    
+     /**
+     * Static class to represent an AntBrain pairing.
+     * @param <X> Type of the first entry.
+     * @param <Y> Type of the second entry.
+     */
+    public static class Pair<X, Y> {
+
+        /**
+         * The first ant brain in the pair.
+         */
+        public final AntBrain one;
+
+        /**
+         * The second ant brain in the pair.
+         */
+        public final AntBrain two;
+        
+        /**
+         * Class constructor for a pair of ant brains.
+         * @param one First ant brain.
+         * @param two Second ant brain.
+         */
+        public Pair(AntBrain one, AntBrain two) {
+            this.one = one;
+            this.two = two;
+        }
     }
     
 }
