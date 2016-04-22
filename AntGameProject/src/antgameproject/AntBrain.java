778f0cb3f812570c9f1744219a7aa0ad062ecc55
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-22 12:16:36 +0100

    Added score fields to the ant brain.

--- AntGameProject/src/antgameproject/AntBrain.java
+++ AntGameProject/src/antgameproject/AntBrain.java
@@ -11,10 +11,24 @@ public class AntBrain {
 
     private List<Instruction> instructions = new ArrayList<>();
     private String name;
+    private int gamesPlayedIn;
+    private int gamesWon;
+    private int gamesDrawn;
+    private int gamesLost;
+    private int totalFoodInBase;
+    private int totalFoodInEnemyBase;
+    private int points;
 
     public AntBrain(List<Instruction> instructions, String name) {
         this.instructions = instructions;
         this.name = name;
+        gamesPlayedIn = 0;
+        gamesWon = 0;
+        gamesDrawn = 0;
+        gamesLost = 0;
+        totalFoodInBase = 0;
+        totalFoodInEnemyBase = 0;
+        points = 0;
     }
 
     public Instruction getInstruction(int state) {
@@ -25,4 +39,61 @@ public class AntBrain {
     public String toString(){
         return name;
     }
+
+    public int getGamesPlayedIn() {
+        return gamesPlayedIn;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public int getGamesDrawn() {
+        return gamesDrawn;
+    }
+
+    public int getGamesLost() {
+        return gamesLost;
+    }
+
+    public int getTotalFoodInBase() {
+        return totalFoodInBase;
+    }
+
+    public int getTotalFoodInEnemyBase() {
+        return totalFoodInEnemyBase;
+    }
+
+    public void incrementGamesPlayedIn() {
+        gamesPlayedIn++;
+    }
+
+    public void incrementGamesWon() {
+        gamesWon++;
+    }
+
+    public void incrementGamesDrawn() {
+        gamesDrawn++;
+    }
+
+    public void incrementGamesLost() {
+        gamesLost++;
+    }
+
+    public void setTotalFoodInBase(int totalFoodInBase) {
+        this.totalFoodInBase = totalFoodInBase;
+    }
+
+    public void setTotalFoodInEnemyBase(int totalFoodInEnemyBase) {
+        this.totalFoodInEnemyBase = totalFoodInEnemyBase;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    
 }
