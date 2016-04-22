e6cdf7c6cfed68262978a5c4616c59b077c92b88
wilki <wilki@DESKTOP-DGB6O1R>
2016-04-22 22:40:18 +0100

    Added documentation to the PosJUnitTest test class.

--- AntGameProject/test/antGameTesting/PosJUnitTest.java
+++ AntGameProject/test/antGameTesting/PosJUnitTest.java
@@ -1,24 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package antGameTesting;
 
 import antgameproject.Pos;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author wilki
+ * Test class for the Pos class.
  */
 public class PosJUnitTest {
     
+    /*
+    * Create an instance on the Pos class and test the accessor methods.
+    */
     @Test
     public void testPosCreation(){
         int x = 3;
