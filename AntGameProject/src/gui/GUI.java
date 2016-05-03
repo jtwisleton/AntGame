7e975de7c8032d8510018d8e54d530150498cb56
josephkimberley <josephkimberley@192.168.1.4>
2016-05-03 16:08:40 +0100

    Javadoc.

--- AntGameProject/src/gui/GUI.java
+++ AntGameProject/src/gui/GUI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import antgameproject.AntGameTournament;
@@ -11,22 +6,38 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 /**
- *
- * @author wilki
+ * Class to represent a Graphical User Interface for the Ant Game.
+ * 
+ * @author team18
  */
 public class GUI extends StateBasedGame {
 
     private AntGameTournament tournament;
     
+    /**
+     * Construct a new GUI object.
+     * 
+     * @param name Name of the State Based Game.
+     * @param tournament Ant game tournament being displayed.
+     */
     public GUI(String name, AntGameTournament tournament) {
         super(name);
         this.tournament = tournament;
     }
-	   
+	      
+    /**
+     * Initialize the list of states.
+     * 
+     * @param container Game container holding the GUI.
+     * @throws SlickException if problem adding states.
+     */
     @Override
     public void initStatesList(GameContainer container) throws SlickException {
+        // Get the size of the screen
         float scale = getScale(container);
         System.out.println(scale);
+        
+        // Add all the screens as states
         this.addState(new GUITeamScreen(scale));
         this.addState(new GUITitleScreen(scale));
         this.addState(new GUIMenuScreen(tournament, scale));
@@ -36,8 +47,16 @@ public class GUI extends StateBasedGame {
         this.addState(new GUISingleGameDisplay(tournament, scale));
     }
 
+    /**
+     * Get the scale of the game container.
+     * 
+     * @param container Game container for measuring.
+     * @return The containers scale as a float.
+     */
     private float getScale(GameContainer container) {
         System.out.println(container.getWidth());
+        
+        // Calculate the scale depending on the users resolution choice
         if(container.getWidth() == 1920){
             return 1;
         } else if(container.getWidth() == 1440){
