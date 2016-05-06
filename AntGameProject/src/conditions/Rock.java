26944aebd2ce1b2789e5d3472b53507a7e9447f9
josephkimberley <josephkimberley@192.168.1.4>
2016-05-06 02:43:17 +0100

    Javadoc.

--- AntGameProject/src/conditions/Rock.java
+++ AntGameProject/src/conditions/Rock.java
@@ -16,7 +16,7 @@ public class Rock implements Condition {
      * Sense a rock in the given position on the given board.
      *
      * @param sensePosition position to sense for rock.
-     * @param antColour
+     * @param antColour Colour of the ant.
      * @param gameBoard board to sense for rock on.
      * @return true if a rock is found at the position given on the board given
      * and false otherwise.
